7243a8e476834ba53939d116f5cb9f309b616833
unknown <jkayecutillas5@gmail.com>
2025-03-25 09:43:34 +0800

    Added User Store API

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserRequest;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
+
+        return $user;
+    }
+
+    //  /**
+    //  * Display the specified resource.
+    //  */
+    // public function show(string $id)
+    // {
+    //     return User::find($id);
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(UserRequest $request, string $id)
+    // {
+    //     $user = User::findORFail($id);
+
+    //     $validated = $request->validated();
+
+    //     $user->name = $validated['name'];
+
+    //     $user->save();
+
+    //     return $user;
+    // }
+
+    // /**
+    //  * Update the email resource in storage.
+    //  */
+    // public function email(UserRequest $request, string $id)
+    // {
+
+    //     $user = User::findORFail($id);
+
+    //     $validated = $request->validated();
+
+    //     $user->email = $validated['email'];
+
+    //     $user->save();
+
+    //     return $user;
+    // }
+
+    // /**
+    //  * Update the password resource in storage.
+    //  */
+    // public function password(UserRequest $request, string $id)
+    // {
+
+    //     $user = User::findORFail($id);
+
+    //     $validated = $request->validated();
+
+    //     $validated['password'] = Hash::make($validated['password']);
+
+    //     $user->save();
+
+    //     return $user;
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(string $id)
+    // {
+    //     $user = User::findORFail($id);
+
+    //     $user->delete();
+
+    //     return $user;
+    // }
+
+    // /**
+    //  * Update the image of the specified resource from storage.
+    //  */
+    // public function image(UserRequest $request, string $id)
+    // {
+    //     $user = User::findORFail($id);
+
+    //     if (!is_null($user->image)) {
+    //         Storage::disk('public')->delete($user->image);
+    //     }
+
+    //     $user->image = $request->file('image')->storePublicly('images', 'public');
+
+
+    //     $user->save();
+
+    //     return $user;
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+Route::get('/user',                 [UserController::class, 'index']);
+Route::get('/user/{id}',            [UserController::class, 'show']);
+Route::post('/user',                [UserController::class, 'store'])->name('user.store');
+Route::put('/user/{id}',            [UserController::class, 'update'])->name('user.update');
+Route::put('/user/email/{id}',      [UserController::class, 'email'])->name('user.email');
+Route::put('/user/password/{id}',   [UserController::class, 'password'])->name('user.password');
+Route::put('/user/image/{id}',      [UserController::class, 'image'])->name('user.image');
+Route::delete('/user/{id}',         [UserController::class, 'destroy']);
+
